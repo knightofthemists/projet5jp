b6c9fcc1caa4203b380fbcbd6e6dfb9ca163c02c
knightofthemists <knightofthemists@hotmail.fr>
2017-08-11 16:46:52 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,20 @@
+<nav class="col-lg-12 navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> 
+        </button>
+        <a class="navbar-brand">
+        <img class="hidden-xs" src="css/images/profil01.jpg" alt="...">
+        <img class="visible-xs" src="css/images/profil02.jpg" alt="...">
+        </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+            <li><a href="index.php">accueil</a></li>
+            <li><a href="liste.php">liste des posts</a></li>
+            <li><a href="creation.php">créez votre post</a></li>  
+        </ul>
+    </div>
+</nav>
