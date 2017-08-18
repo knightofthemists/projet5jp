8d5d34749a52df03cba221cbd0091acff68bd891
knightofthemists <knightofthemists@hotmail.fr>
2017-08-18 11:19:33 +0200

    Add files via upload

---
+++ gabarit.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <!-- Metatags, liens vers Bootstrap et feuilles de styles -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+
+        <!-- css -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+        <div class="container-fluid">
+
+            <div class="row">
+                
+                <?php include("menu.php"); ?>
+
+            </div>
+
+            <div id="contenu">
+                <?= $contenu ?>
+            </div> <!-- #contenu -->
+
+            <?php include("footer.php"); ?>
+
+        </div>
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+    </body>
+
+</html>
