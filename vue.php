126e6184c17ed30e12c0b4ce2a87842fc192ce4d
knightofthemists <knightofthemists@hotmail.fr>
2017-08-18 11:19:52 +0200

    Add files via upload

---
+++ vue.php
@@ -0,0 +1,41 @@
+<?php
+
+class Vue {
+
+  // Nom du fichier associé à la vue
+  private $fichier;
+
+  public function __construct($action) {
+    // Détermination du nom du fichier vue à partir de l'action
+    $this->fichier = "vue/vue" . $action . ".php";
+  }
+
+  // Génère et affiche la vue
+  public function generer($donnees) {
+    // Génération de la partie spécifique de la vue
+    $contenu = $this->genererFichier($this->fichier, $donnees);
+    // Génération du gabarit commun utilisant la partie spécifique
+    $vue = $this->genererFichier('vue/gabarit.php',
+      array('titre' => $this->titre, 'contenu' => $contenu));
+    // Renvoi de la vue au navigateur
+    echo $vue;
+  }
+
+  // Génère un fichier vue et renvoie le résultat produit
+  private function genererFichier($fichier, $donnees) {
+    if (file_exists($fichier)) {
+      // Rend les éléments du tableau $donnees accessibles dans la vue
+      extract($donnees);
+      // Démarrage de la temporisation de sortie
+      ob_start();
+      // Inclut le fichier vue
+      // Son résultat est placé dans le tampon de sortie
+      require $fichier;
+      // Arrêt de la temporisation et renvoi du tampon de sortie
+      return ob_get_clean();
+    }
+    else {
+      throw new Exception("Fichier '$fichier' introuvable");
+    }
+  }
+}
